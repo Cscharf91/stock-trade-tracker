583906886feceece5ac1631a28e4f8a6503861bd
Cscharf91 <cscharf91@gmail.com>
2020-11-13 18:50:32 -0500

    stylesheet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "path to application.css"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
